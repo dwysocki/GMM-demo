1a1b59cacef287a335f5b30c6ea16912cf70cc2f
Daniel Wysocki <dwysocki@oswego.edu>
2016-04-03 16:50:59 -0400

    Fully functioning
    The demonstration appears to be in working order.

--- GMM_demo.py
+++ GMM_demo.py
@@ -1,17 +1,9 @@
-#!/usr/bin/env python3
 import numpy as np
-import matplotlib as mpl
 from matplotlib import pyplot as plt
+from matplotlib.widgets import Slider, Button
 from scipy.stats import multivariate_normal
 
-from matplotlib.widgets import Slider, Button
-
 from functools import reduce
-from itertools import product
-
-from pprint import pprint
-
-
 
 
 def _get_dimn(data, params):
@@ -46,22 +38,6 @@ def multinorm_rvs(size, params):
                                 cov=params["cov"][k])
         for k in range(n_components(params))
     ])
-
-
-def index_product(*indices):
-    """
-    Return an iterable of indices, for all combinations.
-
-    for i, j in index_product(I, J):
-        ...
-
-    is equivalent to
-
-    for i in range(I):
-        for j in range(J):
-            ...
-    """
-    return product(*map(range, indices))
 
 
 def responsibility(data, params,
@@ -123,15 +99,61 @@ def learn(data, params_guess, iterations, dtype=float):
     return params
 
 
+def clear_contours(ax, contours):
+    if contours is None:
+        return
+
+    for coll in contours.collections:
+        ax.collections.remove(coll)
+
+
+class PlotContainer(object):
+    contour = None
+    scatter = None
+
+
 def main():
     np.random.seed(1)
 
-    xmin, xmax = 0, 10
-    ymin, ymax = 0, 10
-    x, y = np.mgrid[0:10:0.01, 0:10:0.01]
+    plt_container = PlotContainer()
+    plt_container.samples = 100
+
+    ## Limits ##
+
+    max_iter = 20
+
+    var_min, var_max = 0.01, 2.00
+    cov_min, cov_max = 0.00, 2.00
+
+    weight_min, weight_max = 0.00, 1.00
+
+    xmin, xmax = 0, 20
+    ymin, ymax = 0, 20
+
+    mean_min, mean_max = 5, 15
+
+    ## Grid points for contours ##
+
+    x, y = np.mgrid[0:20:0.01, 0:20:0.01]
     pos = np.dstack((x,y))
 
-    shape = 11, 5
+    ## Initial Slider Values ##
+
+    init_T_weight = 0.8
+    init_G_weight = 0.5
+
+    init_T_mean_1x, init_T_mean_1y = 7, 7
+    init_T_mean_2x, init_T_mean_2y = 12, 12
+    init_G_mean_1x, init_G_mean_1y = mean_min, mean_min
+    init_G_mean_2x, init_G_mean_2y = mean_max, mean_max
+
+    init_T_cov_1_11, init_T_cov_1_22, init_T_cov_1_12 = 1.25, 1.25, 0.00
+    init_T_cov_2_11, init_T_cov_2_22, init_T_cov_2_12 = 0.50, 0.50, 0.20
+    init_G_cov_1_11, init_G_cov_1_22, init_G_cov_1_12 = 1.00, 1.00, 0.00
+    init_G_cov_2_11, init_G_cov_2_22, init_G_cov_2_12 = 1.00, 1.00, 0.00
+
+    ## Layout ##
+    shape = 14, 5
     height, width = shape
 
     fig, _ = plt.subplots(height, width)
@@ -148,119 +170,185 @@ def main():
     ax_G_mean_2x = plt.subplot2grid(shape, (3, 1))
     ax_G_mean_2y = plt.subplot2grid(shape, (4, 1))
 
-    ax_T_cov_11 = plt.subplot2grid(shape, (5, 0))
-    ax_T_cov_22 = plt.subplot2grid(shape, (6, 0))
-    ax_T_cov_12 = plt.subplot2grid(shape, (7, 0))
-    ax_G_cov_11 = plt.subplot2grid(shape, (5, 1))
-    ax_G_cov_22 = plt.subplot2grid(shape, (6, 1))
-    ax_G_cov_12 = plt.subplot2grid(shape, (7, 1))
+    ax_T_cov_1_11 = plt.subplot2grid(shape, (5, 0))
+    ax_T_cov_1_22 = plt.subplot2grid(shape, (6, 0))
+    ax_T_cov_1_12 = plt.subplot2grid(shape, (7, 0))
+    ax_T_cov_2_11 = plt.subplot2grid(shape, (8, 0))
+    ax_T_cov_2_22 = plt.subplot2grid(shape, (9, 0))
+    ax_T_cov_2_12 = plt.subplot2grid(shape, (10, 0))
+    ax_G_cov_1_11 = plt.subplot2grid(shape, (5, 1))
+    ax_G_cov_1_22 = plt.subplot2grid(shape, (6, 1))
+    ax_G_cov_1_12 = plt.subplot2grid(shape, (7, 1))
+    ax_G_cov_2_11 = plt.subplot2grid(shape, (8, 1))
+    ax_G_cov_2_22 = plt.subplot2grid(shape, (9, 1))
+    ax_G_cov_2_12 = plt.subplot2grid(shape, (10, 1))
 
-    ax_T_weight = plt.subplot2grid(shape, (8, 0))
-    ax_G_weight = plt.subplot2grid(shape, (8, 1))
+    ax_T_weight = plt.subplot2grid(shape, (11, 0))
+    ax_G_weight = plt.subplot2grid(shape, (11, 1))
 
-    ax_iteration = plt.subplot2grid(shape, (9, 0),
+    ax_iteration = plt.subplot2grid(shape, (12, 0),
                                     colspan=2)
-    ax_blank = plt.subplot2grid(shape, (10, 0))
-    ax_blank.axis("off")
-    ax_refit = plt.subplot2grid(shape, (10, 1))
+    ax_blank = plt.subplot2grid(shape, (13, 0))
+    ax_refit = plt.subplot2grid(shape, (13, 1))
 
     ax_plot = plt.subplot2grid(shape, (0, 2),
                                colspan=width-2,
                                rowspan=height)
 
+    ## Labels ##
     ax_T_label.get_xaxis().set_visible(False)
     ax_T_label.get_yaxis().set_visible(False)
     ax_G_label.get_xaxis().set_visible(False)
     ax_G_label.get_yaxis().set_visible(False)
 
-    ax_T_label.text(0.5, 0.5, "True Dist", ha="center", va="center")
-    ax_G_label.text(0.5, 0.5, "Init Guess", ha="center", va="center")
+    ax_T_label.text(0.5, 0.5, "True Dist",
+                    ha="center", va="center")
+    ax_G_label.text(0.5, 0.5, "Init Guess",
+                    ha="center", va="center")
+
+    ## Mean Sliders ##
 
     sl_T_mean_1x = Slider(ax_T_mean_1x, r"$\mu_{1x}$",
-                          xmin, xmax, valinit=xmin)
+                          mean_min, mean_max, valinit=init_T_mean_1x)
     sl_T_mean_1y = Slider(ax_T_mean_1y, r"$\mu_{1y}$",
-                          xmin, ymax, valinit=ymin)
+                          mean_min, mean_max, valinit=init_T_mean_1y)
     sl_T_mean_2x = Slider(ax_T_mean_2x, r"$\mu_{2x}$",
-                          xmin, xmax, valinit=xmin)
+                          mean_min, mean_max, valinit=init_T_mean_2x)
     sl_T_mean_2y = Slider(ax_T_mean_2y, r"$\mu_{2y}$",
-                          xmin, ymax, valinit=ymin)
+                          mean_min, mean_max, valinit=init_T_mean_2y)
     sl_G_mean_1x = Slider(ax_G_mean_1x, r"$\mu_{1x}$",
-                          xmin, xmax, valinit=xmin)
+                          mean_min, mean_max, valinit=init_G_mean_1x)
     sl_G_mean_1y = Slider(ax_G_mean_1y, r"$\mu_{1y}$",
-                          xmin, ymax, valinit=ymin)
+                          mean_min, mean_max, valinit=init_G_mean_1y)
     sl_G_mean_2x = Slider(ax_G_mean_2x, r"$\mu_{2x}$",
-                          xmin, xmax, valinit=xmin)
+                          mean_min, mean_max, valinit=init_G_mean_2x)
     sl_G_mean_2y = Slider(ax_G_mean_2y, r"$\mu_{2y}$",
-                          xmin, ymax, valinit=ymin)
+                          mean_min, mean_max, valinit=init_G_mean_2y)
 
-    sl_T_cov_11 = Slider(ax_T_cov_11, r"$\Sigma_{11}$",
-                          0.01, 2, valinit=1)
-    sl_T_cov_22 = Slider(ax_T_cov_22, r"$\Sigma_{22}$",
-                          0.01, 2, valinit=1)
-    sl_T_cov_12 = Slider(ax_T_cov_12, r"$\Sigma_{12}$",
-                          0.00, 2, valinit=0)
-    sl_G_cov_11 = Slider(ax_G_cov_11, r"$\Sigma_{11}$",
-                          0.01, 2, valinit=1)
-    sl_G_cov_22 = Slider(ax_G_cov_22, r"$\Sigma_{22}$",
-                          0.01, 2, valinit=1)
-    sl_G_cov_12 = Slider(ax_G_cov_12, r"$\Sigma_{12}$",
-                          0.00, 2, valinit=0)
+    ## Covariance Sliders ##
+
+    sl_T_cov_1_11 = Slider(ax_T_cov_1_11, r"$\Sigma^1_{11}$",
+                           var_min, var_max, valinit=init_T_cov_1_11)
+    sl_T_cov_1_22 = Slider(ax_T_cov_1_22, r"$\Sigma^1_{22}$",
+                           var_min, var_max, valinit=init_T_cov_1_22)
+    sl_T_cov_1_12 = Slider(ax_T_cov_1_12, r"$\Sigma^1_{12}$",
+                           cov_min, cov_max, valinit=init_T_cov_1_12)
+    sl_T_cov_2_11 = Slider(ax_T_cov_2_11, r"$\Sigma^2_{11}$",
+                           var_min, var_max, valinit=init_T_cov_2_11)
+    sl_T_cov_2_22 = Slider(ax_T_cov_2_22, r"$\Sigma^2_{22}$",
+                           var_min, var_max, valinit=init_T_cov_2_22)
+    sl_T_cov_2_12 = Slider(ax_T_cov_2_12, r"$\Sigma^2_{12}$",
+                           cov_min, cov_max, valinit=init_T_cov_2_12)
+    sl_G_cov_1_11 = Slider(ax_G_cov_1_11, r"$\Sigma^1_{11}$",
+                           var_min, var_max, valinit=init_G_cov_1_11)
+    sl_G_cov_1_22 = Slider(ax_G_cov_1_22, r"$\Sigma^1_{22}$",
+                           var_min, var_max, valinit=init_G_cov_1_22)
+    sl_G_cov_1_12 = Slider(ax_G_cov_1_12, r"$\Sigma^1_{12}$",
+                           cov_min, cov_max, valinit=init_G_cov_1_12)
+    sl_G_cov_2_11 = Slider(ax_G_cov_2_11, r"$\Sigma^2_{11}$",
+                           var_min, var_max, valinit=init_G_cov_2_11)
+    sl_G_cov_2_22 = Slider(ax_G_cov_2_22, r"$\Sigma^2_{22}$",
+                           var_min, var_max, valinit=init_G_cov_2_22)
+    sl_G_cov_2_12 = Slider(ax_G_cov_2_12, r"$\Sigma^2_{12}$",
+                           cov_min, cov_max, valinit=init_G_cov_2_12)
+
+    ## Weight Sliders ##
 
     sl_T_weight = Slider(ax_T_weight, r"$\pi_1$",
-                         0.00, 1.00, valinit=0.5)
+                         weight_min, weight_max, valinit=init_T_weight)
     sl_G_weight = Slider(ax_G_weight, r"$\pi_1$",
-                         0.00, 1.00, valinit=0.5)
+                         weight_min, weight_max, valinit=init_G_weight)
+
+    ## Iteration Slider ##
 
     sl_iteration = Slider(ax_iteration, r"Iter",
-                          0, 100, valinit=0, valfmt="%d")
+                          0, max_iter, valinit=0, valfmt="%d")
 
+    ## Refit Button ##
+
+    ax_blank.axis("off")
     bu_refit = Button(ax_refit, "Refit")
 
 
+    def parse_params():
+        """
+        Set distribution parameters from slider values.
+        """
+        ## Weights ##
+        T_weight = sl_T_weight.val
+        G_weight = sl_G_weight.val
+        weight_true  = np.array([T_weight, 1-T_weight])
+        weight_guess = np.array([G_weight, 1-G_weight])
+        ## Means ##
+        mean_true  = np.array([
+            [sl_T_mean_1x.val, sl_T_mean_1y.val],
+            [sl_T_mean_2x.val, sl_T_mean_2y.val]
+        ])
+        mean_guess = np.array([
+            [sl_G_mean_1x.val, sl_G_mean_1y.val],
+            [sl_G_mean_2x.val, sl_G_mean_2y.val]
+        ])
+        ## Cov's ##
+        cov_true  = np.array([
+            [[sl_T_cov_1_11.val, sl_T_cov_1_12.val],
+             [sl_T_cov_1_12.val, sl_T_cov_1_22.val]],
+
+            [[sl_T_cov_2_11.val, sl_T_cov_2_12.val],
+             [sl_T_cov_2_12.val, sl_T_cov_2_22.val]]
+        ])
+        cov_guess = np.array([
+            [[sl_G_cov_1_11.val, sl_G_cov_1_12.val],
+             [sl_G_cov_1_12.val, sl_G_cov_1_22.val]],
+
+            [[sl_G_cov_2_11.val, sl_G_cov_2_12.val],
+             [sl_G_cov_2_12.val, sl_G_cov_2_22.val]]
+        ])
+        ## Store values in container ##
+        plt_container.params_true  = dict(
+            weight=weight_true,
+            mean=mean_true,
+            cov=cov_true
+        )
+        plt_container.params_guess = dict(
+            weight=weight_guess,
+            mean=mean_guess,
+            cov=cov_guess
+        )
+
+    def plot_contours():
+        i = int(sl_iteration.val)
+        plt_container.contour = \
+          ax_plot.contour(x, y,
+                          multinorm_pdf(pos, plt_container.params_learned[i]))
+
     def update_true(*args, **kwargs):
+        parse_params()
+        data = multinorm_rvs(plt_container.samples, plt_container.params_true)
+        plt_container.params_learned = learn(data,
+                                             plt_container.params_guess,
+                                             max_iter)
+
+        if plt_container.scatter is not None:
+            plt_container.scatter.set_offsets(data)
+        else:
+            plt_container.scatter = ax_plot.scatter(*(data.T))
+
+        update_fit() # calls fig.canvas.draw_idle(), so no need to call here
+
+    def update_fit(*args, **kwargs):
+        clear_contours(ax_plot, plt_container.contour)
+        plot_contours()
         fig.canvas.draw_idle()
 
-    sl_T_mean_1x.on_changed(update_true)
+    bu_refit.on_clicked(update_true)
+    sl_iteration.on_changed(update_fit)
 
-    # Parameters of true distribution.
-    weight_true = np.array([0.2, 0.8])
-    mean_true = np.array([[7, 7],
-                          [2, 2]])
-    cov_true = np.array([[[1, 0],
-                          [0, 1]],
+    update_true()
 
-                         [[2, 1],
-                          [1, 2]]])
-    params_true = dict(weight=weight_true,
-                       mean=mean_true,
-                       cov=cov_true)
+    ax_plot.set_xlim([xmin, xmax])
+    ax_plot.set_ylim([ymin, ymax])
+    fig.tight_layout(h_pad=0.5, w_pad=0.5)
 
-    data = multinorm_rvs(100, params_true)
-
-    # Initial guess for parameters
-    weight_guess = np.array([0.5, 0.5])
-    mean_guess = np.array([[2, 2],
-                           [7, 7]])
-    cov_guess = np.array([[[1, 0],
-                           [0, 1]],
-
-                          [[1, 0],
-                           [0, 1]]])
-    params_guess = dict(weight=weight_guess,
-                        mean=mean_guess,
-                        cov=cov_guess)
-
-    ax_plot.scatter(*(data.T))
-    ax_plot.contour(x, y, multinorm_pdf(pos, params_guess))
-
-    params_learned = learn(data, params_guess, 100)[-1]
-
-    print("True Distribution:")
-    pprint(params_true)
-    print("Learned Distribution:")
-    pprint(params_learned)
-
-    fig.tight_layout(h_pad=0.5, w_pad=0.2)
 
     plt.show(block=True)
 
